e963cbd9b3f03ee6da87821627b6518387d55472
Avijit Ghosh <avijit@Avijits-MacBook-Air.local>
2025-04-26 01:08:53 +0530

    Binary Search Day 3

---
+++ Striver's A2Z Sheet/Binary Search/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,30 @@
+// Problem Link: https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+class Solution {
+    public:
+        int search(vector<int>& nums, int target) {
+            int n = nums.size(), low = 0, high = n-1;
+    
+            while(low <= high){
+                int mid = low + (high - low)/2;
+    
+                if(nums[mid] == target) return mid;
+                else if(nums[low] <= nums[mid]){ // left sorted
+                    if(nums[low] <= target && target <= nums[mid]) {
+                        high = mid - 1;
+                    }
+                    else low = mid + 1;
+                }
+                else{ // right sorted
+                    if(nums[mid] <= target && target <= nums[high]){
+                        low = mid + 1;
+                    }
+                    else{
+                        high = mid - 1;
+                    }
+                }
+            }
+    
+            return -1;
+        }
+    };
